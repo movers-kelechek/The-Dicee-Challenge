cc9a6abdd417232456b0721e62f2c1e4506f982f
Baktygul Kozhoshova <baktygul.kozhoshova.kk@gmail.com>
2021-10-26 13:59:29 +0600

    both players random

--- index.js
+++ index.js
@@ -24,18 +24,18 @@ switch(randomNumber1){
    
 
 
+let randomNumber2=Math.floor(Math.random()*6)+1;
 
-
-/*if(randomNumber1===1){
-     b.setAttribute("src", "/images/dice1.png");
-}else if(randomNumber1===2){
-   b.setAttribute("src", "/images/dice2.png");
-}else if(randomNumber1===3){
-   b.setAttribute("src", "/images/dice3.png");
-}else if(randomNumber1===4){
-   b.setAttribute("src", "/images/dice4.png");
-}else if(randomNumber1===5){
-   b.setAttribute("src", "/images/dice5.png");
-}else if(randomNumber1===6){
-   b.setAttribute("src", "/images/dice6.png");
-}*/
+if(randomNumber2===1){
+   document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+}else if(randomNumber2===2){
+   document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+}else if(randomNumber2===3){
+   document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+}else if(randomNumber2===4){
+   document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+}else if(randomNumber2===5){
+   document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+}else if(randomNumber2===6){
+   document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+}
